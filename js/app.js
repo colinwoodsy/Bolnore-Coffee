d3e661ed87010c4f064502f3755ea8d419a18f56
Colin Woods <20406351+colinwoodsy@users.noreply.github.com>
2019-01-08 16:37:07 +0000

    add question transitions with some refactoring

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var app = new Vue({
   el: '#app',
   data: {
-  	show: true,
+  	showThankYou: false,
   	showButtons: false,
+  	showTextInput: false,
+  	textAnswer: '',
   	appearAfter0ms: false,
   	appearAfter300ms: false,
   	appearAfter800ms: false,
@@ -35,17 +37,28 @@ var app = new Vue({
   methods: {
   	answerQuestion (answer = 1) { 		
   		this.answers[this.questionIndex] = answer
+  		this.showButtons = false  		
+  		this.showTextInput = false
+  		if (this.questionIndex >= this.questions.length-1) {
+  			this.questionIndex = this.questions.length
+  			setTimeout(() => this.showThankYou = true, 800)
+  			return
+  		}
+  		if (this.questionIndex === 3) {
+  			this.questionIndex  = -1  			
+  			setTimeout(() => this.questionIndex = 4, 800)
+	  		setTimeout(() => this.showTextInput = true, 1200)
+	  		return
+	  	}  		
   		tmpQuestionIndex = this.questionIndex + answer
   		this.questionIndex  = -1
   		setTimeout(() => this.questionIndex = tmpQuestionIndex, 800)
   		setTimeout(() => this.showButtons = true, 1200)
-  		this.showButtons = false
-  		// this.transitionOut = true
   	}
   },
   mounted () {
   	this.appearAfter0ms = true
-  	setTimeout(() => this.showButtons = true, 200)
+  	setTimeout(() => this.showButtons = true, 300)
   	setTimeout(() => this.appearAfter300ms = true, 200)
   	setTimeout(() => this.appearAfter800ms = true, 600)
   }
