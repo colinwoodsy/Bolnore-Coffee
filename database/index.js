cf88997cc835cbd66182ace2754eeaf9c2906f74
Colin Woods <20406351+colinwoodsy@users.noreply.github.com>
2019-01-21 21:10:16 +0000

    refactor database initialisation

--- database/index.js
+++ database/index.js
@@ -1,15 +1,36 @@
 const sys = require('sys')
 var sqlite3 = require('sqlite3').verbose();
 var file = __dirname + "/dbs/bolnorecoffee";
-var db = new sqlite3.Database(file);
+
+// https://github.com/mapbox/node-sqlite3/issues/674
+var db = new sqlite3.Database(file, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (err) {
+  if (err) console.log("SQL Lite error", err.message);
+});
 
 // create table if it does not already exist
-db.run("CREATE TABLE IF NOT EXISTS answers ('question1' TEXT, 'question2' TEXT, 'question3' TEXT, 'question4' TEXT, 'question5' TEXT, 'datestamp' Date, 'ip+address' TEXT, 'browser_agent' TEXT)")
+let sqlCreateCheck = "CREATE TABLE IF NOT EXISTS answers ('question1' TEXT, 'question2' TEXT, 'question3' TEXT, 'question4' TEXT, 'question5' TEXT, 'question_datestamp' Date, 'ip_address' TEXT, 'browser_agent' TEXT)"
+db.run(sqlCreateCheck, (err, rows) => {
+  if (err) {
+    console.log("sqlCreateCheck Error", err)
+    throw err;
+  }
+  // selectCheck()
+});
+
+function selectCheck () {
+  let sqlSelect = 'SELECT * FROM answers'
+  console.log("sqlSelect", sqlSelect)
+  db.all(sqlSelect, [], (err, rows) => {
+    console.log("rows", rows)
+    if (err) {
+      console.log("sqlSelect Error", err)
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
 
 
-db.all("SELECT * FROM answers", function(err, rows) {
-        rows.forEach(function (row) {
-            console.log(row);
-        })
-	});	
-db.close();
+module.exports = db;
