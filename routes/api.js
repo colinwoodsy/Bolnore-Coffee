e6cff0aa28138672265bb56db84eda641180e651
Colin Woods <20406351+colinwoodsy@users.noreply.github.com>
2019-01-21 21:12:34 +0000

    refactor post answers, save to database

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../database');
+const express = require('express');
+const router = express.Router();
+const db = require('../database');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/answers', function(req, res, next) {
+  db.all('SELECT * FROM answers', [], (err, rows) => res.send(rows))	
 });
 
 router.post('/answers', function(req, res, next) {
-  res.send('respond with a resource');
+	let answers = req.body.answers
+	let ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+	let values = [...answers.map(x => x.answer), Date.now(), ipAddress, req.useragent.source]
+	db.run(`INSERT INTO answers (question1, question2, question3, question4, question5, question_datestamp, ip_address, browser_agent) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, 
+		values, 
+		function(err) {
+			if (err) {
+				return console.log(err.message);
+				res.send(err.message);
+			}
+			// get the last insert id
+			res.send(`A row has been inserted with rowid ${this.lastID}`);
+		}
+	);
+
 });
+
+// db.close();
 
 module.exports = router;
